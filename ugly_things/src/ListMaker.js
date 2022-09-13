e78a3c7f669fee8d496390c1eb53602239a73194
Jon Zaro <writetojonzaro@gmail.com>
2022-09-13 11:02:55 -0700

    ugly Things delete

--- ugly_things/src/ListMaker.js
+++ ugly_things/src/ListMaker.js
@@ -1,26 +1,49 @@
-import React, {useContext, useState} from "react"
+import React, {useContext, useState, useTransition} from "react"
 import { UglyContext } from "./uglyThingsProvider"
+import axios from "axios"
 
 
 function ListMaker(){
 
     const contextThings = useContext(UglyContext)
     
-    // const [allThings, setAllThings] = React.useState();
-    //use effect to run GET request with a dependency array, 
-    //
-        //access state value prop and map through it to render component
-    //    const list = "variablename".allThings.map()
 
-    // React.useContext()
+    const deleteItem = (id) => {
+        console.log("Delete item with this ID: ", id)
+        axios.delete(`https://api.vschool.io/jonzaro/thing/${id}`)
+        .then(res => {
+            console.log("Delete request: ", res)
+            UglyContext.addNewItem(prev => {
+                //if this is the incorrect way to access context and get my state setter funciton, then how come the axios 
+                //delete is working?
+                return prev.filter((item) => item._id !== id)
+                
+                //also where do I refresh page inside delete button
+                
+            })
+        })
+        .catch(err => console.log(err))
 
+    }
+
+    const list = contextThings.allThings.map(thing => <div key={thing._id}>
+        <h2>{thing.title}</h2>
+        <img src={thing.imgUrl} />
+        <br></br>
+        {thing.description}
+        <br></br>
+        <button onClick={() => {deleteItem(thing._id)}}>Delete</button>
+        <hr></hr>
+    </div>
+    
+)
     return (
         
         <div className="list">
             {console.log("From ListMaker.js", contextThings.allThings)}
-
-            <h3>ListMaker component returns items from API</h3>
+            {list}
         </div>
+            // {/* /* ListMaker component returns items from API */ */}
         
 
     )
